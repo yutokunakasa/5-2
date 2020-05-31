9691c3cbe68b348695ccbbdb7f817ce7c3704c65
仲佐　祐德 <yutokunakasa@nakatasukuyuunoMacBook-Air.local>
2020-05-31 18:51:13 +0900

    [start]5-3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '95937c7f7a1821df76f7d850b6b71b15ced3d5fb234fcfe4079b7c0f41f48eea4af86f805206b6f3b133feb3b31005c8d7df031fd12415fb27d9011d1a01a51f'
