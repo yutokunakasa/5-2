93f5df196231e17a9e8c0643ea1bea591a8a1a76
仲佐　祐德 <yutokunakasa@nakatasukuyuunoMacBook-Air.local>
2020-06-01 23:23:35 +0900

    [update]5-5,5-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'post_images#index'
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   	resource :favorites, only:[:create, :destroy]
   	resources :post_comments, only: [:create, :destroy]
   end
+  resources :users,only: [:show, :edit, :update]
 end
